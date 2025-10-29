086ef6efac5039bc365ef0eedf6d2a1dead0c42a
ahmetsefacetin <ahmet80sefa@hotmail.com>
2025-10-29 13:33:35 +0300

    edited gitignore file

--- UniversityStudentCommunityApplicationBackend/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ UniversityStudentCommunityApplicationBackend/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c808727c2aaca095b3c92077cbe8ab78b22a3a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4653372b1917e2212e726305d9668bd95b23567")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
